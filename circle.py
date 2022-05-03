ff67cfd34933e98ba626dd21cc95800a00d72c34
Van Huu Chinh <huuchinh98bg1@gmail.com>
2022-05-03 20:44:43 +0700

    add circle

---
+++ circle.py
@@ -0,0 +1,10 @@
+import turtle
+sc = turtle.Screen()
+print(sc)
+turtle.pensize(5)
+turtle.pencolor("pink")
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.circle(50)
+turtle.end_fill()
+turtle.done
