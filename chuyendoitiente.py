cf989bfdf394b556336f05e127bfee9ba7e969c8
Van Huu Chinh <huuchinh98bg1@gmail.com>
2022-05-23 14:06:47 +0700

    add chuyen doi tien te

---
+++ chuyendoitiente.py
@@ -0,0 +1,15 @@
+usd = int(input("nhap so tien USD: "))
+vnd = usd * 22950
+aud = usd * 1.35
+nzd = usd * 1.42
+eur = usd * 0.84
+jpy = usd * 108.99
+cny = usd * 6.47
+
+print("Đổi tiền:")
+print (str(usd) + " usd = " + str(vnd) + " vnd")
+print (str(usd) + " usd = " + str(aud) + " aud")
+print (str(usd) + " usd = " + str(nzd) + " nzd")
+print (str(usd) + " usd = " + str(eur) + " eur")
+print (str(usd) + " usd = " + str(jpy) + " jpy")
+print (str(usd) + " usd = " + str(cny) + " cny")
